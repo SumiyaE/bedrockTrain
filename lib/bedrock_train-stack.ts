9a8d056904ce7174dfee430df3e8525b572ef533
江口純矢 <s_eguchi@codmon.co.jp>
2024-06-23 15:18:08 +0900

    lambdaの型情報をnpm install

--- lib/bedrock_train-stack.ts
+++ lib/bedrock_train-stack.ts
@@ -1,16 +1,17 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
+import {NodejsFunction} from "aws-cdk-lib/aws-lambda-nodejs";
+import {Runtime} from "aws-cdk-lib/aws-lambda";
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class BedrockTrainStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'BedrockTrainQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const lambda =  new NodejsFunction(this, 'BedrockTrainLambda', {
+        entry: 'lambda/bedrock_train.ts',
+        handler: 'handler',
+        runtime: Runtime.NODEJS_20_X
+    })
   }
 }
