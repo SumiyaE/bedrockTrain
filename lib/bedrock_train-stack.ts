3a4bf49ba2e8c4f6e8f2de3de4e1a95d1fa820f7
江口純矢 <s_eguchi@codmon.co.jp>
2024-06-23 18:37:49 +0900

    フォーマット

--- lib/bedrock_train-stack.ts
+++ lib/bedrock_train-stack.ts
@@ -1,18 +1,16 @@
 import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
+import {Construct} from 'constructs';
 import {NodejsFunction} from "aws-cdk-lib/aws-lambda-nodejs";
 import {Architecture, Runtime} from "aws-cdk-lib/aws-lambda";
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class BedrockTrainStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
-    super(scope, id, props);
-
-  const lambda =  new NodejsFunction(this, 'BedrockTrainLambda', {
-      entry: 'lambda/index.ts',
-      handler: 'handler',
-      runtime: Runtime.NODEJS_LATEST,
-      architecture:Architecture.ARM_64,
-  })
-}
+    constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+        super(scope, id, props);
+        const lambda = new NodejsFunction(this, 'BedrockTrainLambda', {
+            entry: 'lambda/index.ts',
+            handler: 'handler',
+            runtime: Runtime.NODEJS_LATEST,
+            architecture: Architecture.ARM_64,
+        })
+    }
 }
