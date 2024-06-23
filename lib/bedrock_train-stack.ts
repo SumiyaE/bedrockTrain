3aa4dc91b783208ae394606e84d8a8de91559011
江口純矢 <s_eguchi@codmon.co.jp>
2024-06-23 19:04:44 +0900

    bedrockへの権限を最小構成に変更

--- lib/bedrock_train-stack.ts
+++ lib/bedrock_train-stack.ts
@@ -2,28 +2,24 @@ import * as cdk from 'aws-cdk-lib';
 import {Construct} from 'constructs';
 import {NodejsFunction} from "aws-cdk-lib/aws-lambda-nodejs";
 import {Architecture, Runtime} from "aws-cdk-lib/aws-lambda";
-import {Effect, PolicyStatement, Role} from "aws-cdk-lib/aws-iam";
+import {Effect, PolicyStatement} from "aws-cdk-lib/aws-iam";
 
 export class BedrockTrainStack extends cdk.Stack {
     constructor(scope: Construct, id: string, props?: cdk.StackProps) {
         super(scope, id, props);
+        const bedRockAccessPolicy = new PolicyStatement({
+            effect: Effect.ALLOW,
+            actions: ['bedrock:InvokeModel'],
+            resources: ['*'],
+        })
+
         const lambda = new NodejsFunction(this, 'BedrockTrainLambda', {
             entry: 'lambda/index.ts',
             handler: 'handler',
             runtime: Runtime.NODEJS_LATEST,
             architecture: Architecture.ARM_64,
+            initialPolicy: [bedRockAccessPolicy],
             timeout: cdk.Duration.seconds(30),
         })
-
-        // 自前の IAM ポリシー作成
-        const bedRockAccessPolicy = new PolicyStatement({
-            effect: Effect.ALLOW,
-            actions: ['bedrock:*'],
-            resources: ['*'],
-        })
-
-        // IAM ロールにアタッチ
-        const lambdaRole = lambda.role as Role;
-        lambdaRole.addToPolicy(bedRockAccessPolicy);
     }
 }
