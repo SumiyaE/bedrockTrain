dd47faf625fea79e9f2ca037fbce2a3d20b2b609
江口純矢 <s_eguchi@codmon.co.jp>
2024-06-23 18:48:06 +0900

    ポリシーを編集

--- lib/bedrock_train-stack.ts
+++ lib/bedrock_train-stack.ts
@@ -2,6 +2,7 @@ import * as cdk from 'aws-cdk-lib';
 import {Construct} from 'constructs';
 import {NodejsFunction} from "aws-cdk-lib/aws-lambda-nodejs";
 import {Architecture, Runtime} from "aws-cdk-lib/aws-lambda";
+import {Effect, PolicyStatement, Role} from "aws-cdk-lib/aws-iam";
 
 export class BedrockTrainStack extends cdk.Stack {
     constructor(scope: Construct, id: string, props?: cdk.StackProps) {
@@ -12,5 +13,16 @@ export class BedrockTrainStack extends cdk.Stack {
             runtime: Runtime.NODEJS_LATEST,
             architecture: Architecture.ARM_64,
         })
+
+        // 自前の IAM ポリシー作成
+        const bedRockAccessPolicy = new PolicyStatement({
+            effect: Effect.ALLOW,
+            actions: ['bedrock-runtime:*'],
+            resources: ['*'],
+        })
+
+        // IAM ロールにアタッチ
+        const lambdaRole = lambda.role as Role;
+        lambdaRole.addToPolicy(bedRockAccessPolicy);
     }
 }
