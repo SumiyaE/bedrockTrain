d669a3f1ee71c95ac40c7f3e1a5780eec17cf353
江口純矢 <s_eguchi@codmon.co.jp>
2024-06-23 16:17:17 +0900

    bedrockにリクエストを送信できるようにソースコードを編集

--- lambda/index.ts
+++ lambda/index.ts
@@ -1,7 +1,41 @@
 import { Handler } from 'aws-lambda';
+import {
+    BedrockRuntimeClient,
+    ConverseCommand,
+    Message
+} from "@aws-sdk/client-bedrock-runtime";
 
 export const handler: Handler = async (event, context) => {
     console.log('EVENT: \n' + JSON.stringify(event, null, 2));
     console.log('hello this is lambda message')
-    return context.logStreamName;
+
+
+
+    const client = new BedrockRuntimeClient({ region: "us-east-1" });
+    const modelId = "anthropic.claude-v2:1";
+    const userMessage = "これはユーザーのメッセージです。Amazon Bedrock モデルの使い方を教えてください";
+
+    const conversation:Message[] = [
+        {
+            role: "user",
+            content: [{ text: userMessage }],
+        },
+    ];
+    // 他のコマンドとの違いがわからない
+    const command = new ConverseCommand({
+        modelId,
+        messages: conversation,
+        inferenceConfig: { maxTokens: 512, temperature: 0.5, topP: 0.9 },
+    });
+
+    try {
+        // Send the command to the model and wait for the response
+        const response = await client.send(command);
+
+        // Extract and print the streamed response text in real-time.
+        const responseText = response.output?.message?.content?.[0].text ?? 'No response text available';
+    } catch (err) {
+        console.log(`ERROR: Can't invoke '${modelId}'. Reason: ${err}`);
+    }
+
 };
